4c211443087e9394e8ec2c9e7b93f40e0580c8cc
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-08-11 20:12:44 -0300

    Visual Representation of Merge Sort Exercise 3 Completed

--- Ordenacao/Exercicio_3.cpp
+++ Ordenacao/Exercicio_3.cpp
@@ -7,7 +7,50 @@ subvetores.
 
 // ----------------------------------------------------------------- //
 
+▶ Começa a Divisão dentro da função recursiva:
+    [6, 9, 1, 8, 15, 4, 13, 9, 2, 2]
+    [6, 9, 1, 8, 15][4, 13, 9, 2, 2] 
+    [6, 9][1, 8, 15][4, 13][9, 2, 2]
+    [6][9][1][8, 15][4][13][9][2, 2]
+    [6][9][1][8][15][4][13][9][2][2]
 
+▶ Agora que a parte recursiva já dividiu todo o vetor, ele "volta fechando", ou seja, intercalado
+
+    Intercala [2] e [2]
+    Ordenado = [2, 2]
+    Resultado = [6, 9, 1, 8, 15, 4, 13, 9, 2, 2]
+
+    Intercala [8, 15]
+    Ordenado = [6, 15]
+    Resultado = [6, 9, 1, 8, 15, 4, 13, 9, 2, 2]
+
+    Intercala [9] e [2, 2]
+    Ordenado = [2, 2, 9]
+    Resultado = [6, 9, 1, 8, 15, 4, 13, 2, 2, 9]
+
+    Intercala [1] e [8, 15]
+    Ordenado = [1, 8, 15]
+    Resultado = [6, 9, 1, 8, 15, 4, 13, 2, 2, 9]
+
+    Intercala [4] e [13]
+    Ordenado = [4, 13]
+    Resultado = [6, 9, 1, 8, 15, 4, 13, 2, 2, 9]
+
+    Intercala [6] e [9]
+    Ordenado = [6, 9]
+    Resultado = [6, 9, 1, 8, 15, 4, 13, 2, 2, 9]
+
+    Intercala [4, 13] e [2, 2, 9]
+    Ordenado = [2, 2, 4, 9, 13]
+    Resultado = [6, 9, 1, 8, 15, 2, 2, 4, 9, 13]
+
+    Intercala [6, 9] e [1, 8, 15]
+    Ordenado = [1, 6, 8, 9, 15]
+    Resultado = [1, 6, 8, 9, 15, 2, 2, 4, 9, 13]
+
+    Intercala [1, 6, 8, 9, 15] e [2, 2, 4, 9, 13]
+    Ordenado = [1, 2, 2, 4, 6, 8, 9, 9, 13, 15]
+    Resultado = [1, 2, 2, 4, 6, 8, 9, 9, 13, 15]
 
 */
 
